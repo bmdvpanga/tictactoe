9447efed52427d0e6b3259b302fdb243744c7b0a
christopher-wolff23 <cwolff12@umd.edu>
2019-01-14 04:01:44 -0500

    Attempted to place all the buttons in a python list. Tried to regsiter an event listener for all buttons. Trying text in buttons instead of images, at the moment

--- xo-gui.py
+++ xo-gui.py
@@ -2,23 +2,44 @@ from flexx import flx
 
 class Example(flx.Widget):
 
+
+    
+
     def init(self):
-        with flx.HSplit():
+
+        self.buttons = []
+
+        with flx.HSplit() as self.all:
             with flx.VSplit():
                 with flx.HBox():
-                    flx.Button(style='background:yellow;', flex=1) 
-                    flx.Button(style='background:red;', flex=1) 
-                    flx.Button(style='background:blue;', flex=1)
+                    for i in range(3):
+                        self.buttons[i] = flx.Button(style='background:red;', flex=1) 
+
                 with flx.HBox():
-                    flx.Button(style='background:yellow;', flex=1) 
-                    flx.Button(style='background:red;', flex=1) 
-                    flx.Button(style='background:blue;', flex=1)
+                    for i in range(4,7):
+                        self.buttons[i] = flx.Button(style='background:green;', flex=1) 
+
                 with flx.HBox():
-                    flx.Button(style='background:yellow;', flex=1) 
-                    flx.Button(style='background:red;', flex=1) 
-                    flx.Button(style='background:blue;', flex=1)
+                    for i in range(7,10):
+                        self.buttons[i] = flx.Button(style='background:blue;', flex=1) 
+
+    #Was going to use this method to dispose of the component listening on the event
+    #and replace it with a new one, but this seems to be discouraged. Especially considering the 
+    #fact this is replacing the component which is listening for events
+    @flx.action
+    def replace_button_with_image(self):
+        #self.button1 = flx.ImageWidget(flex=1, style='max-width:50px; max-height:50px', 
+        #source='https://github.com/bmdvpanga/tictactoe/bob/master/images/cat.jpg?raw=true')
+        for i in self.buttons:
+            self.buttons[i].set_text("x")
+
+
+    @flx.reaction('all.children*.pointer_click')
+    def but2_clicked(self, *events):
+       self.replace_button_with_image()
+
 
 app = flx.App(Example)
-app.export('example.html', link=0)  # Export to single file
-app.launch('browser')  # show it now in a browser
+app.export('tic-tac-toe.html', link=0)  # Export to single file
+app.launch('browser')  # show it now) in a browser
 flx.run()  # enter the mainloop

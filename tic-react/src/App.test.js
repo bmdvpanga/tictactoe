5886701dd7e1c905a7025625d42cf7d53ac93296
Christopher Wolff <cwolff12@umd.edu>
2019-01-22 02:05:44 -0500

    can now make 'moves' by registering an event listener to a component

--- tic-react/src/App.test.js
+++ tic-react/src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './TicTacToe';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<TicTacToe />, div);
   ReactDOM.unmountComponentAtNode(div);
 });

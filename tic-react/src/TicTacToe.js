bfe86faf18f67c4ca8fd5db52c6489f42f80b27a
Christopher Wolff <cwolff12@umd.edu>
2019-01-22 01:23:24 -0500

    Added some styling to be able to see the border of elements. TicTacToe and Tile are now working components

--- tic-react/src/TicTacToe.js
+++ tic-react/src/TicTacToe.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'; //don't quite understand the {} syntax of this import statement 
+import Tile from './Tile';
 import './TicTacToe.css';
 
+/*
+  This Component/Class is will be the parent component
+  for a Tile, and it will represent the TicTacToe game board
+
+*/
 class TicTacToe extends Component {
+
+  //What's being rendered, the board, is just a 3x3 table of Tiles
   render() {
     return (
       <div className="TicTacToe">
-        
+        <table>
+            <tr>
+                <td><Tile>Tile Placeholder</Tile></td>
+                <td><Tile>Tile Placeholder</Tile></td>
+                <td><Tile>Tile Placeholder</Tile></td>
+            </tr>
+            <tr>
+                <td><Tile>Tile Placeholder</Tile></td>
+                <td><Tile>Tile Placeholder</Tile></td>
+                <td><Tile>Tile Placeholder</Tile></td>
+            </tr>
+            <tr>
+                <td><Tile>Tile Placeholder</Tile></td>
+                <td><Tile>Tile Placeholder</Tile></td>
+                <td><Tile>Tile Placeholder</Tile></td>
+            </tr>
+                
+        </table>
       </div>
     );
   }
---
+++ tic-react/src/Tile.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './TicTacToe.css';
+
+class Tile extends Component {
+  render() {
+
+    
+    return (
+      <div className="Tile">
+      </div>
+    );
+  }
+}
+
+export default Tile;

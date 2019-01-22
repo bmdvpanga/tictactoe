ce5ee86339209224612e034d69fbd33e33d0d852
Christopher Wolff <cwolff12@umd.edu>
2019-01-22 00:52:00 -0500

    ran create-react-app and generated some boilerplate React code/markup. Figuring out what kind of component are needed.

---
+++ tic-react/src/TicTacToe.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './TicTacToe.css';
+
+class TicTacToe extends Component {
+  render() {
+    return (
+      <div className="TicTacToe">
+        
+      </div>
+    );
+  }
+}
+
+export default TicTacToe;

6f3c57f7f9c1e0ef5a1ab28c24a06c0095d77776
Christopher Wolff <cwolff12@umd.edu>
2019-01-25 01:52:40 -0500

    finally figured out CORS issue (installed a flash extension and had the back end make sure everything was CORS safe). Can successful send JSON to React app on front end!

--- tic-react/src/CreateGame.js
+++ tic-react/src/CreateGame.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Requests from './requests';
+import { PROXY_URL } from './requests';
 
 class CreateGame extends React.Component{
 
@@ -12,7 +12,7 @@ class CreateGame extends React.Component{
     requestNewGameFromTTTServer(event){
         console.log("requestNewGameFromTTTServer() was called");
         //fetch requests wraps everything in a promise
-        fetch(proxyurl + 'http://127.0.0.1:5000/')
+        fetch(/*PROXY_URL +*/ 'http://127.0.0.1:5000/')
         .then(response => response.json())
         .then(json => console.log(json))
         .catch(); //looks like a catch function can take an arrow function lambda thing as a parameter

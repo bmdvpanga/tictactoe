bac5954859a3ea3e71de6872e4a7cf9635acb646
Christopher Wolff <cwolff12@umd.edu>
2019-01-25 01:52:40 -0500

    finally figured out CORS issue (installed a flash extension and had the back end make sure everything was CORS safe). Can successful send JSON to React app on front end!

--- hello.py
+++ hello.py
@@ -1,9 +1,15 @@
-import json
 
 '''flask web server and controller'''
+
+import json #flask also has JSON support, but this seems to be classical way to do JSON in Python
 from flask import Flask
+from flask_cors import CORS, cross_origin
+
+
 app = Flask(__name__)
-print("can send message to console")
+CORS(app) #CORS broswer prevention issue
+
+print("can send message to console outside of method")
 
 #testing flask
 @app.route('/')
@@ -12,6 +18,11 @@ def hello_world():
     #try to see if we can print a message to the console when the endpoint is hit
     print(message)
     json_message = json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}])
+   
+    #COMMENTED OUT CODE WHEN TRYING TO FIX THE BROWSER ISSUE    
+    #json_message.headers.add('Access-Control-Allow-Origin', '*') 
+    #COMMENTED OUT CODE
+   
     print(json_message)
     return json_message
 
--- tic-react/src/requests.js
+++ tic-react/src/requests.js
@@ -1,7 +1,10 @@
 /*Constants for making requests will be stored here, so can be used by fetch
-in other files*/
+in other files. */
 
 /*Apparently the browser blocks certain requests from going through if they lack something called a 
 /Access-Control-Allow-Origin header. 
 /https://stackoverflow.com/questions/43871637/no-access-control-allow-origin-header-is-present-on-the-requested-resource-whe*/
-const proxyurl = "https://cors-anywhere.herokuapp.com/";
+
+
+
+export const PROXY_URL = "https://cors-anywhere.herokuapp.com/";

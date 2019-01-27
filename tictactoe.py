b718569acb998ede2e151c71eece73d7aff0b42c
Maria Vivar-Guzman <cwolff12@umd.edu>
2019-01-27 01:08:49 -0500

    created a logic.py file and a folder for back-end unit tests

--- tictactoe.py
+++ tictactoe.py
@@ -4,8 +4,6 @@
     In current implementation two players make a move at the same computer.
 '''
 
-
-
 #player constants
 PLAYER_X = "X"
 PLAYER_O = "O"
@@ -34,14 +32,6 @@ def makeMove(board, curPlayer):
         print("Not valid move, try again")
 
 
-'''Modular code which is useful outside of a local two player game'''
-
-#check if valid move
-def isValidMove(board, move):
-    if (move not in board.values()):
-        return False
-    else:
-        return True
 
 #switches from one player to the other
 def playerSwitch(curPlayer):
@@ -50,42 +40,6 @@ def playerSwitch(curPlayer):
     else:
         curPlayer = PLAYER_X
     return curPlayer
-
-#helper function for checking if the row of a board results in a win
-def checkRowWin(board):
-    if ( (board.get("1") == board.get("2") == board.get("3")) or
-         (board.get("4") == board.get("5") == board.get("6")) or
-         (board.get("7") == board.get("8") == board.get("9"))):
-         return True
-    return False
-
-#helper function for checking if the collumn of a board results in a win
-def checkCollumnWin(board):
-    if ((board.get("1") == board.get("4") == board.get("7")) or
-    (board.get("2") == board.get("5") == board.get("8")) or
-    (board.get("3") == board.get("6") == board.get("9"))):
-        return True
-    return False
-
-#helper function for checking if the diagonal of a board results in a win
-def checkDiagonalWin(board):
-    if ((board.get("1") == board.get("5") == board.get("9")) or
-    (board.get("3") == board.get("5") == board.get("7"))):
-        return True
-    return False
-
-'''check if winning condition is met, return boolean value
-current implementation requires two checks, one for each player...'''
-def checkWin(board):
-    #win condition -- need to make more efficient...
-    if (checkRowWin(board) is True or
-    checkCollumnWin(board) is True or
-    checkDiagonalWin(board) is True):
-         return True
-    return False
-
-
-'''End Mondular Code'''
 
 
 #driver function, a local game will still send information to the server to check 

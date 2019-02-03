fd9c2a5935129ee8d2e53f934f6b1a1953c4acd9
Christopher Wolff <cwolff12@umd.edu>
2019-02-03 18:25:46 -0500

    Renamed some components. Made CreateGame a child component of GameModeMenu. Added a constructor for the Game class and a static counter for figuring out how many Game objects have been created, thus far.

--- server_and_game_logic/game.py
+++ server_and_game_logic/game.py
@@ -1,1 +1,18 @@
-'''Will contains the Game board state'''
+'''Will contains the all of the state for the back-end of the application -- and any
+state needed for representing a tic-tac-toe game'''
+
+#player constants
+PLAYER_X = "X"
+PLAYER_O = "O"
+
+class Game:
+    gameCount = 0 #a python 'static variable' is not tied to an instance 
+
+    def __init__(self, gameType, board = {'1': '1', '2': '2', '3': '3',
+        '4': '4', '5': '5', '6': '6',
+        '7': '7', '8': '8', '9': '9'}, currentPlayer = PLAYER_X):
+        self.board = board
+        self.currentPlayer = currentPlayer
+        self.gameType = gameType
+        Game.gameCount +=1
+

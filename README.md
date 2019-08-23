d9077342a82fc36cdca13b722e4a6fa592a15258
Christopher Wolff <cwolff12@umd.edu>
2019-08-23 17:54:12 -0400

    Updated the readme, added a new folder and file for streamlining developement.

--- README.md
+++ README.md
@@ -8,11 +8,12 @@ Tic-Tac-Toe with a Python/Flask server back-end for controlling the program logi
 1) Ensure `python3` and `pip3` module installer is installed on the user's local machine
 2) Fork/clone this repo
 
-### Installation instructions for running the front-end for testing/debugging: ###
+### Installation instructions for running the "front-end" for testing/debugging: ###
 
 1) Install NodeJS (stable release) via different operating system instructions
 2) In the repository folder named `tic-react` run the command `npm install.` This will install the React modules and other Node.js dependencies
-3) Run the front-end server with the command `npm start` in the folder named `tic-react`
+3) Ensure you are using the most recent version of NodeJS with the command `npm update` in the folder named `tic-react.`
+4) Run the "front-end" server with the command `npm start` in the folder named `tic-react`
 
 
 ### Installation instructions for running the back-end of the program for testing/debugging (Flask web server): ###
@@ -23,10 +24,10 @@ If using Powershell on windows, use the command `$env:FLASK_APP="controller.py"`
 3) Install Flask CORS (for preventing the browser from stopping Ajax requests) with `pip install -U flask-cors`
 
 ### A Note on CORS
-Certain browsers will not allow certain communication between the React app and Flask server if they are on different ports of the same localmachine (`localhost`). It is considered suspicious activity by the browser. (The Flask server is supposed to workaround this with step 3, but there is a [problem](https://github.com/bmdvpanga/tictactoe/issues/19).) As a hotfix, you can run Google (the reccomended browser for testing this application) with CORS features disabled. To run chrome with command line arguments you can simply add `chrome.exe` to your environment variables/PATH, and in the terminal enter the command `chrome --disable-web-security`.
+Certain browsers will not allow certain communication between the React app and Flask server if they are on different ports of the same localmachine (`localhost`). It is considered suspicious activity by the browser. (The Flask server is supposed to workaround this with step 3, but there is a [problem](https://github.com/bmdvpanga/tictactoe/issues/19).) As a hotfix, you can run Chrome (the reccomended browser for testing this application) with CORS features disabled. To run Chrome with command line arguments you can simply add `chrome.exe` to your environment variables/PATH, and in the terminal enter the command `chrome --disable-web-security`.
 
 4) (Optional) Alias with `set FLASK_ENV=development` (Windows) to avoid rerunning `flask run` on modified changes. (React will automatically recompile on changes.)
-5) Run the command `flask run` in the folder `server_and_game_logic`. 
+5) Run the back-end server with the command `flask run` in the folder `server_and_game_logic`. 
 6) Instructions for testing endpoints with Postman/Swagger will go here...
 
 
--- server_and_game_logic/controller.py
+++ server_and_game_logic/controller.py
@@ -1,17 +1,20 @@
-'''Flask web server and controller. Stateless as possible.'''
+'''Flask web server and controller. Stateless as possible.
+TODO: 
+    1. research the endpoint for PUT so that it operates on the same hash
+    2. flask via internet for remote testing'''
 
 #Flask also has JSON support, but this seems to be classical way to do JSON in Python.
 import json 
 from flask import Flask
 from flask import abort
-from flask import request #request has a different context within each method
+from flask import request #Request has a different context within each endpoint.
 from flask_cors import CORS, logging
 
 #Import Game class from the Game file.
 from game import Game
 
 app = Flask(__name__)
-#CORS broswer prevention issue, start the Flask appplication. I believe this should only be a problem when testing because trying to send requests between two different ports on localhost.
+#CORS broswer prevention issue, start the Flask appplication. This should only be a problem when testing because trying to send requests between two different ports on localhost.
 CORS(app) 
 logging.getLogger('flask_cors').level = logging.DEBUG
 
@@ -21,7 +24,7 @@ games = {} #Indices start at 1.
 #Testing flask sever is up
 @app.route('/')
 def test_server():
-    #Try to see if we can print a test json object message to the console when the endpoint is hit from the front-end
+    #Try to see if we can print a test json object message to the console when the endpoint is hit from the front-end.
     json_message = json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}])
     return json_message
    
@@ -32,21 +35,15 @@ def createNewGame():
     #If the method is GET, then return the entire games dictionary.
     if(request.method == "GET"):
         json_message = json.dumps(games, default=lambda o: o.__dict__)
-
-    #If the method is POST, then only return the game that was just created as json
-    elif(request.method == "POST"):
+    elif(request.method == "POST"): #If the method is POST, then only return the game that was just created as JSON.
         # This always creates a new game and gameCount is increased in the constructor by 1.
         print ("hit the post in the /games")
-        games[Game.gameCount] = Game(request.args.get('gameMode','')) # Currently there is an inconsistancy between the current game on the front-end and the gameCount.
+        games[Game.gameCount] = Game(request.args.get('gameMode','')) # Currently there is an inconsistancy between the current game on the front-end, and the gameCount. TODO: Verify if this is still true.
         json_message = json.dumps(games[Game.gameCount], default=lambda o: o.__dict__) # Returns string representation of JSON game object.
         
-    #This gnarly bit of code is so that we can also see the gameCount in the JSON message. Only replace first bracket.   
+    #This gnarly bit of code is so that we can also see the gameCount in the JSON message. Only replaces the first bracket.   
     json_message = json_message.replace("{", "{\"gameCount\": " + str(Game.gameCount) + ", ", 1) 
     return json_message              
-
-    '''TODO: 
-        1. research the endpoint for PUT so that it operates on the same hash
-        2. flask via internet for remote testing'''
 
 #records the move that is made by the user through PUT request. unfinished for 'best practice' leave verbs out of the url
 #https://blog.mwaysolutions.com/2014/06/05/10-best-practices-for-better-restful-api/
